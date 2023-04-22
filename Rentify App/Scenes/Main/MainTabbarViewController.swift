3a885e4d24ba95ac51c065fe827a77cfdc9bb8ec
instasanjik <koshkarbayev.07@gmail.com>
2023-04-22 14:20:24 +0600

    feat (main): add tabbar config

--- Rentify App/Scenes/Main/MainTabbarViewController.swift
+++ Rentify App/Scenes/Main/MainTabbarViewController.swift
@@ -11,6 +11,7 @@ class MainTabbarViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureViewControllers()
         updateTabbarUI()
         selectedIndex = 2
     }
@@ -21,7 +22,7 @@ class MainTabbarViewController: UITabBarController {
 extension MainTabbarViewController {
     
     func updateTabbarUI() {
-        tabBar.tintColor = ColorPalette.yellow
+        tabBar.tintColor = ColorPalette.white
         tabBar.barTintColor = .clear
         tabBar.backgroundImage = UIImage()
         tabBar.shadowImage = UIImage()
@@ -41,13 +42,28 @@ extension MainTabbarViewController {
             cornerRadius: height / 2)
         
         roundLayer.path = bezierPath.cgPath
-        roundLayer.fillColor = ColorPalette.white.cgColor
+        roundLayer.fillColor = ColorPalette.black.cgColor
         
         tabBar.layer.insertSublayer(roundLayer, at: 0)
         
         tabBar.itemWidth = width / 5.5
         tabBar.itemPositioning = .centered
         tabBar.unselectedItemTintColor = ColorPalette.systemGray
+    }
+    
+    func configureViewControllers() {
+        let vc = self.storyboard?.instantiateViewController(withIdentifier: "FavoritesNavigationViewController")
+        let vc1 = self.storyboard?.instantiateViewController(withIdentifier: "HousesNavigationViewController")
+        
+        let vc2 = self.storyboard?.instantiateViewController(withIdentifier: "MainNavigationViewController")
+    
+        let vc3 = self.storyboard?.instantiateViewController(withIdentifier: "ProfileNavigationViewController")
+        viewControllers = [
+            prepareViewController(vc!, imageName: "Heart"),
+            prepareViewController(vc1!, imageName: "House"),
+            prepareViewController(vc2!, imageName: "Search"),
+            prepareViewController(vc3!, imageName: "User")
+        ]
     }
     
     private func prepareViewController(_ viewController: UIViewController, title: String? = nil, imageName: String) -> UIViewController {
