088ce2871ff29085fbd0ad1a41aa7b6c481a744c
instasanjik <koshkarbayev.07@gmail.com>
2023-04-25 12:02:37 +0600

    feat (main): add table view files

---
+++ Rentify App/Scenes/Main/ViewControllers/HouseScene/TableViewCells/HouseCalendarTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  HouseCalendarTableViewCell.swift
+//  Rentify App
+//
+//  Created by Sanzhar Koshkarbayev on 25.04.2023.
+//
+
+import UIKit
+
+class HouseCalendarTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Rentify App/Scenes/Main/ViewControllers/HouseScene/TableViewCells/HouseHeaderTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  HouseHeaderTableViewCell.swift
+//  Rentify App
+//
+//  Created by Sanzhar Koshkarbayev on 25.04.2023.
+//
+
+import UIKit
+
+class HouseHeaderTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
