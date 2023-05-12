b2842b7f4a55154f3b563414d0755bf15eb3dfef
instasanjik <koshkarbayev.07@gmail.com>
2023-05-13 04:47:19 +0600

    feat (scenes): add house gallery counter

--- Rentify App/Scenes/Main/ViewControllers/HouseScene/TableViewCells/HouseGalleryTableViewCell.swift
+++ Rentify App/Scenes/Main/ViewControllers/HouseScene/TableViewCells/HouseGalleryTableViewCell.swift
@@ -8,6 +8,10 @@
 import UIKit
 
 class HouseGalleryTableViewCell: UITableViewCell {
+    
+    var photoCount = 10
+    var isPlusNeeded = false
+    let PHOTO_DISPLAY_LIMIT = 3
 
     @IBOutlet weak var galleryCollectionView: UICollectionView!
     
@@ -31,12 +35,32 @@ class HouseGalleryTableViewCell: UITableViewCell {
 extension HouseGalleryTableViewCell: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 5
+        if photoCount > PHOTO_DISPLAY_LIMIT {
+            isPlusNeeded = true
+            return PHOTO_DISPLAY_LIMIT
+        }
+        return photoCount
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "GalleryCollectionViewCell", for: indexPath)
-        return cell
+        if indexPath.row == PHOTO_DISPLAY_LIMIT - 1 && isPlusNeeded {
+            let view = UIView(frame: cell.bounds)
+            view.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.5)
+            view.layer.cornerRadius = 4
+            view.clipsToBounds = true
+            cell.addSubview(view)
+            let label = UILabel(frame: view.bounds)
+            label.textAlignment = .center
+            label.text = "+\(photoCount - PHOTO_DISPLAY_LIMIT)"
+            label.textColor = .white
+            label.font = .systemFont(ofSize: 16, weight: .medium)
+            view.addSubview(label)
+            return cell
+        } else {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "GalleryCollectionViewCell", for: indexPath)
+            return cell
+        }
     }
     
     
