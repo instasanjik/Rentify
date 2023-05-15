5bacadc697338f15164f81cd0af24ca9a6cb340f
instasanjik <koshkarbayev.07@gmail.com>
2023-05-15 19:47:14 +0600

    refactor (custom-controls): setup folders

--- Rentify App/Scenes/Main/ViewControllers/FavoritesScene/FavoritesViewController.swift
+++ Rentify App/Scenes/Main/ViewControllers/FavoritesScene/FavoritesViewController.swift
@@ -16,6 +16,16 @@ class FavoritePromise {
     var landlordPhoneNumber: String = ""
     var landlordEmail: String = ""
     
+    
+    init(previewLink: String, price: String, additionalInfo: String, address: String, region: String, landlordPhoneNumber: String, landlordEmail: String) {
+        self.previewLink = previewLink
+        self.price = price
+        self.additionalInfo = additionalInfo
+        self.address = address
+        self.region = region
+        self.landlordPhoneNumber = landlordPhoneNumber
+        self.landlordEmail = landlordEmail
+    }
 }
 
 class FavoritesViewController: UIViewController {
