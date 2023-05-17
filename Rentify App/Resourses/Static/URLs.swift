e054e203217d76ca99428822a7443638e270dceb
instasanjik <koshkarbayev.07@gmail.com>
2023-05-17 09:28:05 +0600

    feat (resourses): update url file

--- Rentify App/Resourses/Static/URLs.swift
+++ Rentify App/Resourses/Static/URLs.swift
@@ -10,5 +10,15 @@ import Foundation
 class URLs {
     static let reportProblemAPI = "http://192.168.0.169:1234/send"
     
-    static let getHouseFull = ""
+    static let SERVER_IP = "http://192.168.0.169:8135"
+    
+    static let LOGIN = SERVER_IP + "/auth/sign_in"
+    static let SIGN_UP = SERVER_IP + "/auth/sign-up"
+    
+    static let GET_ALL_HOUSES = SERVER_IP + "/apartments/"
+    static let GET_HOUSE_BY_ID = SERVER_IP + "/apartments/"
+    
+    static let GET_MY_PROFILE = SERVER_IP + "/users/me"
+    static let UPLOAD_AVATAR = SERVER_IP + "/users/me/upload"
+    static let UPLOAD_PROFILE = SERVER_IP + "/users/me"
 }
